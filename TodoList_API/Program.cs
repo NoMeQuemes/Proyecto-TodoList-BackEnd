38fb47a10f3223ad6041e86ef7ba534fc397d0a0
NoMeQuemes <munozfernando264@gmail.com>
2024-05-03 20:06:00 -0300

    Versión 1.0.0

--- TodoList_API/Program.cs
+++ TodoList_API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,16 +10,35 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Acá se agrega el contexto de la base de datos y se define el nombre de la cadena de conexion
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+});
+//--------------------------------------------------------------------------------------------
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "NuevaPolitica", policy =>
+    {
+        policy.SetIsOriginAllowed(_ => true)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//}
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
+
+app.UseCors("NuevaPolitica");
 
 app.UseAuthorization();
 
